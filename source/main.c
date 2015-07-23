5a337913bf5fee95841b648584b61353ef898638
Manuel Limones <limonesu.me@gmail.com>
2015-07-23 04:49:55 -0500

    Add Counter functions

--- source/main.c
+++ source/main.c
@@ -5,9 +5,25 @@
  */
 void setToMaxSpeed(void);
 
+/**
+ * Initialized the timer 2 as a down counter triggering from the ETR pin
+ * Also enables the update interrupt for this timer
+ * @param upToValue number of counts to issue and iterrupt
+ */
+void timer_2_init(int upToValue);
+
+/**
+ * Initialize Pin B1 as output
+ */
+void led_init(void);
+
 int main(void){
 	setToMaxSpeed();
+	led_init();
+	timer_2_init(5);
 	while(1){
+		for(int i=0; i<0xFFFFF;i++);
+		GPIO_WriteBit(GPIOA,GPIO_Pin_1,!GPIO_ReadOutputDataBit(GPIOA,GPIO_Pin_1));//toggles led
 	}
 }
 
@@ -27,5 +43,49 @@ void setToMaxSpeed(void){
 			break;
 		}
 	}
+}
+
+void timer_2_init(int upToValue){
+	//Configure Pin A0 as TIM2_CH1_ETR
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA,ENABLE);
+	GPIO_InitTypeDef myGPIO;
+	GPIO_StructInit(&myGPIO);
+	myGPIO.GPIO_Pin=GPIO_Pin_0;
+	myGPIO.GPIO_Mode=GPIO_Mode_AF;
+	myGPIO.GPIO_PuPd=GPIO_PuPd_UP;
+	GPIO_Init(GPIOA,&myGPIO);
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource0,GPIO_AF_2);
+	
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2,ENABLE);
+	TIM_TimeBaseInitTypeDef myTimer;
+	TIM_TimeBaseStructInit(&myTimer);
+	myTimer.TIM_CounterMode=TIM_CounterMode_Down;
+	myTimer.TIM_Prescaler=(0);
+	myTimer.TIM_ClockDivision=TIM_CKD_DIV1;
+	myTimer.TIM_Period=(upToValue-1);
+	TIM_TimeBaseInit(TIM2,&myTimer);
+	TIM_ETRClockMode2Config(TIM2,TIM_ExtTRGPSC_OFF,TIM_ExtTRGPolarity_NonInverted,0);
+	TIM_ITConfig(TIM2,TIM_IT_Update,ENABLE);
+	NVIC_EnableIRQ(TIM2_IRQn);
+	
+	TIM_Cmd(TIM2,ENABLE);
+}
+
+void led_init(void){
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB,ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA,ENABLE);
+	GPIO_InitTypeDef myGPIO;
+	GPIO_StructInit(&myGPIO);
+	myGPIO.GPIO_Pin=GPIO_Pin_1;
+	myGPIO.GPIO_Mode=GPIO_Mode_OUT;
+	GPIO_Init(GPIOB,&myGPIO);
+	GPIO_ResetBits(GPIOB,GPIO_Pin_1);
+	GPIO_Init(GPIOA,&myGPIO);
+	GPIO_ResetBits(GPIOA,GPIO_Pin_1);
+}
+
+void TIM2_IRQHandler(void){
+	TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
+	GPIO_WriteBit(GPIOB,GPIO_Pin_1,!GPIO_ReadOutputDataBit(GPIOB,GPIO_Pin_1));//toggles led
 }
 
